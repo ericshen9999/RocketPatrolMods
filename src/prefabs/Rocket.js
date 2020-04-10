1fa75ceaa7ce6737c7f91bca9c821d2df2d56a63
ericshen9999 <dragoneshen@gmail.com>
2020-04-10 15:50:53 -0700

    Added Rocket and Spaceship

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,33 @@
+//Rocket prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene,x,y,texture,frame){
+        super(scene,x,y,texture,frame);
+        //Add objects
+        scene.add.existing(this);
+        this.isFiring = false;
+    }
+
+    update(){
+        if(!this.isFiring){
+            if(keyLEFT.isDown && this.x >= 47){
+                this.x -= 2;
+            } else if (keyRIGHT .isDown && this.x <= 578)
+            {
+                this.x += 2;
+            }
+        }
+        //Fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF)){
+            this.isFiring= true;
+        }
+        //If fired
+        if(this.isFiring && this.y >= 108){
+            this.y -=2;
+        }
+        //Reset
+        if(this.y <= 108){
+            this.isFiring = false;
+            this.y = 431;
+        }
+    }
+}
---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,18 @@
+//Spaceship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene,x,y,texture,frame,pointValue){
+        super(scene,x,y,texture,frame);
+        //Add objects
+        scene.add.existing(this);
+        this.points = pointValue;
+    }
+
+    update(){
+        //Move Left
+        this.x -= 3;
+        //Wraparound
+        if(this.x <= 0 - this.width){
+            this.x = game.config.width;
+        }
+    }
+}
